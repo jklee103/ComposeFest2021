1265ae0fb17e05ea8091ab63cd4d1e69a361c315
jk <>
2021-12-02 16:56:03 +0900

    1-2

--- week 1-Jetpack Compose basics/ComposeTestApp/app/src/main/java/com/example/composetestapp/MainActivity.kt
+++ week 1-Jetpack Compose basics/ComposeTestApp/app/src/main/java/com/example/composetestapp/MainActivity.kt
@@ -3,15 +3,15 @@ package com.example.composetestapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composetestapp.ui.theme.ComposeTestAppTheme
+import kotlin.math.exp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,22 +23,76 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+@Composable
+fun MyApp(){
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+    } else {
+    Greetings()}
+}
 
 @Composable
-private fun MyApp(){
-    Surface(color = MaterialTheme.colors.background) {
-        Greeting(name = "Android")
+private fun Greetings(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) Greeting(name = name)
     }
 }
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit){
+
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    ComposeTestAppTheme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) { //background color
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp)) //modifier가 뭐,,이것저것 지정
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if(expanded.value) 48.dp else 0.dp
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) { //background color
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }// modifier = 뭐,,이것저것 지정
+            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
-    MyApp()
+    ComposeTestAppTheme {
+        Greetings()
+    }
 }
